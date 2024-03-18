696dbff06fd592139bb49736628add0e395301b2
ace <bendrorasaf@gmail.com>
2024-03-18 19:34:17 +0200

    18.3

---
+++ get_api.py
@@ -0,0 +1,19 @@
+import requests
+
+endpoint_url = 'https://4danimals.com/wp-json/wp/v2/posts'
+
+
+response = requests.get(endpoint_url)
+
+
+if response.status_code == 200:
+    
+    json_data = response.json()
+    
+    
+    print(json_data)
+else:
+    
+    print('Error:', response.status_code)
+
+print (response.status_code)
